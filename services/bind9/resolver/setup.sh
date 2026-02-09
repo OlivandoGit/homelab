91d7c63fd814aa0a04366368fd39bd788937c0d8
OlivandoGit <72449057+OlivandoGit@users.noreply.github.com>
2026-02-09 15:29:55 +0000

    moved bind9 back to local config after network outage

--- services/bind9/resolver/setup.sh
+++ services/bind9/resolver/setup.sh
@@ -3,9 +3,6 @@
 #List of ssh clients for my core servers (as defined in my ssh config file)
 vms=("core-01" "core-02")
 
-rm -r /mnt/docker_volumes/bind9-resolver/config/*
-cp config/* /mnt/docker_volumes/bind9-resolver/config/
-
 for i in "${vms[@]}"
 do
     echo "====================================================="
@@ -13,6 +10,26 @@ do
     echo "====================================================="
 
     ssh $i << EOF
+
+    scp -r config $i:~
+
+    ssh $i << EOF
+        if [ ! -d /etc/bind/resolver ]; then
+            sudo mkdir /etc/bind/resolver
+        else
+            sudo rm -r /etc/bind/resolver/*
+        fi
+
+        if [ ! -d /var/bind/authoratatiive ]; then
+            sudo mkdir -p /var/bind/resolver
+        else
+            sudo rm -r /var/bind/resolver/*
+        fi
+
+        sudo mv ~/config/* /etc/bind/resolver/named.conf
+        sudo rm -r ~/config
+
+        sudo rm -r ~/config
         #Turn off the DNSStubListener for this VM
         sudo sed -i 's/#DNSStubListener=yes/DNSStubListener=no/g' /etc/systemd/resolved.conf
         sudo systemctl restart systemd-resolved.service
